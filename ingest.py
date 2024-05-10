281594b770eb2f612248ae5af7f64894efdf2859
veelocity <vaishnavit.006@gmail.com>
2024-05-10 19:26:59 +0530

    added model

--- ingest.py
+++ ingest.py
@@ -1,16 +1,27 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PypDFLoader, DirectoryLoader
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
 
-DATA_PATH = "data/"
-DB_FAISS_PATH = "vectorstores/db_faiss"
+DATA_PATH = 'data/'
+DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-#create vector database
+# Create vector database
 def create_vector_db():
-    loader = DirectoryLoader(DATA_PATH, glob='*.pdf', loader_cls=PypDFLoader)
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
-    textx = text_splitter.split_documents(documents)
+    loader = DirectoryLoader(DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
 
-    embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                                   chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(DB_FAISS_PATH)
+
+if __name__ == "__main__":
+    create_vector_db()
